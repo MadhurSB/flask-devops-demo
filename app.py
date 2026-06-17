b3cc7008129b18a5fa1815fbdc0f15d0a30e9957
Ubuntu <ubuntu@ip-172-31-42-14.eu-north-1.compute.internal>
2026-06-17 15:49:41 +0000

    Run app

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hello World")
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Run App') {
+            steps {
+                sh 'python3 app.py'
+            }
+        }
+    }
+}
